55952acfa880f0a7e829723bb55e66cc74ab1c15
SayidRadhin <pmradhin@gmail.com>
2021-09-29 05:32:48 +0530

    used userEffect function and updating

--- src/Counter.js
+++ src/Counter.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 
 function Counter() {
+    const [count,setCount]=useState(0)
+    const [count2,setCount2]=useState(0)
+    useEffect(()=>{
+        console.log('Mounting...');
+        console.log('count1...'+count);
+        console.log('count2...'+count2);
+        
+    },[count,count2])
+  
     return (
         <div>
-            <h1>Hello i am a component</h1>
+            <button onClick={()=>setCount(count+1)}>Increment</button>
+            <h1>Hello i am a component{count}</h1>
+
+            <button onClick={()=>setCount2(count2+1)}>Increment</button>
+            <h1>Hello i am a component2{count2}</h1>
         </div>
     )
 }
