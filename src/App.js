c0fcf836d30bcc7712c36e594f81e5790262c2a2
SayidRadhin <pmradhin@gmail.com>
2021-10-04 15:54:58 +0530

    take a authentication

--- src/App.js
+++ src/App.js
@@ -4,9 +4,23 @@ function App() {
   return (
     <div className="App">
       <button onClick={()=>{
+        Firebase.auth().createUserWithEmailAndPassword('pmradhin@gmail.com', '123456')
+        .then((userCredential) => {
+          // Signed in 
+          var user = userCredential.user;
+          // ...
+          
           Firebase.firestore().collection('products').doc('WBQqBEJSIh3Kz3mAIKtt').set({
-            name:'updated'
-          })
+            name:'updated' 
+             })
+        })
+        .catch((error) => {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // ..
+        });
+      
+
           
         
       }}>Click me</button>
